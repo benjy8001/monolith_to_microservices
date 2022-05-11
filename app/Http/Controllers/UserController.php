f1efe638f77a97919a067a4a3c8d7aafef07f7b7
benjy8001 <benjy80@gmail.com>
2022-05-11 11:13:19 +0200

    Add Rest routes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,11 +4,67 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
     public function index(): string
     {
         return User::all();
+    }
+
+    /**
+     * @param int $id
+     * @return mixed
+     */
+    public function show(int $id)
+    {
+        return User::find($id);
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request): Response
+    {
+        $user = User::create([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return response($user, Response::HTTP_CREATED);
+    }
+
+    /**
+     * @param Request $request
+     * @param int $id
+     * @return Response
+     */
+    public function update(Request $request, int $id): Response
+    {
+        $user = User::find($id);
+        $user->update([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return response($user, Response::HTTP_ACCEPTED);
+    }
+
+    /**
+     * @param int $id
+     * @return Response
+     */
+    public function destroy(int $id): Response
+    {
+        User::destroy($id);
+
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('users', [UserController::class, 'index']);
+Route::get('users/{id}', [UserController::class, 'show']);
+Route::put('users/{id}', [UserController::class, 'update']);
+Route::delete('users/{id}', [UserController::class, 'destroy']);
+Route::post('users', [UserController::class, 'store']);
