e3a60a67f01586c979eaac258e4146fc17f729da
benjy8001 <benjy80@gmail.com>
2022-05-11 11:19:16 +0200

    Use apiResource

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,8 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('users', [UserController::class, 'index']);
-Route::get('users/{id}', [UserController::class, 'show']);
-Route::put('users/{id}', [UserController::class, 'update']);
-Route::delete('users/{id}', [UserController::class, 'destroy']);
-Route::post('users', [UserController::class, 'store']);
+Route::apiResource('users', UserController::class);
