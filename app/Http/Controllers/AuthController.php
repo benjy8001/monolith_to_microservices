f92f9dfa1bf7528ebddff9017df065efbf2f481b
benjy8001 <benjy80@gmail.com>
2022-05-12 20:53:32 +0200

    Add Middleware & Fix HasApiToken

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,17 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return array|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
     public function login(Request $request)
     {
         if (Auth::attempt($request->only('email', 'password'))) {
             $user = Auth::user();
-            $token = $user->createToken('admin')->accessToken->token;
+            $token = $user->createToken('admin')->accessToken;
 
             return compact('token');
         }
@@ -20,5 +27,19 @@ class AuthController extends Controller
         return response([
             'error' => 'Invalid credentials!'
         ], Response::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * @param RegisterRequest $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create(
+            $request->only('first_name', 'last_name', 'email') +
+            ['password' => Hash::make($request->input('password'))]
+        );
+
+        return response($user, Response::HTTP_CREATED);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('login', [AuthController::class, 'login']);
-Route::apiResource('users', UserController::class);
+Route::post('register', [AuthController::class, 'register']);
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::apiResource('users', UserController::class);
+});
