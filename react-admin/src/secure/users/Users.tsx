c5613fb21a499815fbfc5fbfe2d15c2be8d1ca88
Benjamin MABILLE <benjamin.mabille@businessdecision.com>
2022-05-23 16:03:53 +0200

    React: Add Users list and Role

---
+++ react-admin/src/secure/users/Users.tsx
@@ -0,0 +1,66 @@
+import React, {Component} from "react";
+import Wrapper from "./Wrapper";
+import axios from "axios";
+import {User} from "../../classes/User";
+
+class Users extends Component {
+    state = {
+        users: []
+    }
+
+    componentDidMount = async () => {
+        const response = await axios.get('users');
+        this.setState({
+            users: response.data.data
+        });
+    }
+
+    render() {
+        return (
+            <Wrapper>
+                <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 col-md-3 border-bottom">
+                    <div className="btn-toolbar mb-2 mb-md-0">
+                        <a href="#" className="btn btn-sm btn-outline-secondary">Add</a>
+                    </div>
+                </div>
+
+                <h2>Section title</h2>
+                <div className="table-responsive">
+                    <table className="table table-striped table-sm">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Role</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {this.state.users.map(
+                            (user: User) => {
+                                return (
+                                    <tr>
+                                        <td>{user.id}</td>
+                                        <td>{user.first_name} {user.last_name}</td>
+                                        <td>{user.email}</td>
+                                        <td>{user.role.name}</td>
+                                        <td>
+                                            <div className="btn-group mr-2">
+                                                <a href="#" className="btn btn-sm btn-outline-secondary">Edit</a>
+                                                <a href="#" className="btn btn-sm btn-outline-secondary">Delete</a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                )
+                            }
+                        )}
+                        </tbody>
+                    </table>
+                </div>
+            </Wrapper>
+        )
+    }
+}
+
+export default Users;
