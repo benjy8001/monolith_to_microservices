29ffac3f4a0fd84167fbc165b8558ade4787b7e2
benjy8001 <benjy80@gmail.com>
2022-05-24 22:09:41 +0200

    React: Add links

--- react-admin/src/secure/components/Menu.tsx
+++ react-admin/src/secure/components/Menu.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Menu = () => (
     <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
         <div className="position-sticky pt-3">
             <ul className="nav flex-column">
                 <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">
+                    <NavLink to={'/'} className="nav-link" aria-current="page">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                              stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
                              className="feather feather-home align-text-bottom" aria-hidden="true">
@@ -13,7 +14,20 @@ const Menu = () => (
                             <polyline points="9 22 9 12 15 12 15 22"></polyline>
                         </svg>
                         Dashboard
-                    </a>
+                    </NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to={'/users'} className="nav-link" aria-current="page">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                             className="feather feather-users align-text-bottom" aria-hidden="true">
+                            <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                            <circle cx="9" cy="7" r="4"></circle>
+                            <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
+                            <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+                        </svg>
+                        Users
+                    </NavLink>
                 </li>
             </ul>
         </div>
