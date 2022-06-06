03090fba5835ef05213603faba933efc54f9cd51
benjy8001 <benjy80@gmail.com>
2022-06-06 22:22:19 +0200

    Api: Add Product search

--- backend/app/Http/Controllers/Influencer/ProductController.php
+++ backend/app/Http/Controllers/Influencer/ProductController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers\Influencer;
 
+use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController
 {
-    public function index()
+    public function index(Request $request)
     {
-        return Product::all();
+        $query = Product::query();
+        if ($s = $request->input('s')) {
+            $query->whereRaw(sprintf("title LIKE '%%%s%%'", $s))
+                ->orWhereRaw(sprintf("description LIKE '%%%s%%'", $s));
+        }
+
+        return ProductResource::collection($query->get());
     }
 }
