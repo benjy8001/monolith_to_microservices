568fc55d6afa1b007692ea3a314d1345454ed33d
benjy8001 <benjy80@gmail.com>
2022-06-08 21:25:02 +0200

    Api: Filtering cached datas

--- backend/app/Http/Controllers/Influencer/ProductController.php
+++ backend/app/Http/Controllers/Influencer/ProductController.php
@@ -6,7 +6,9 @@ use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
 
 class ProductController
 {
@@ -16,15 +18,18 @@ class ProductController
      */
     public function index(Request $request): AnonymousResourceCollection
     {
-        return Cache::remember('products', 60*30, function () use ($request) {
-            $query = Product::query();
-            if ($s = $request->input('s')) {
-                $query->whereRaw(sprintf("title LIKE '%%%s%%'", $s))
-                    ->orWhereRaw(sprintf("description LIKE '%%%s%%'", $s));
-            }
-
-            return ProductResource::collection($query->get());
+        /** @var Collection $products */
+        $products = Cache::remember('products', 60*30, function () use ($request) {
+            return Product::all();
         });
+
+        if ($s = $request->input('s')) {
+            $products = $products->filter(function (Product $product) use ($s) {
+                return (Str::contains($product->title, $s) || Str::contains($product->description, $s));
+            });
+        }
+
+        return ProductResource::collection($products);
 
     }
 }
