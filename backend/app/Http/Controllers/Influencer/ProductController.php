d44f210d1e4dff59c7fbf6c86191090ef094f2dc
benjy8001 <benjy80@gmail.com>
2022-06-08 21:04:56 +0200

    Api: Use Redis cache

--- backend/app/Http/Controllers/Influencer/ProductController.php
+++ backend/app/Http/Controllers/Influencer/ProductController.php
@@ -6,6 +6,7 @@ use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\Cache;
 
 class ProductController
 {
@@ -15,12 +16,16 @@ class ProductController
      */
     public function index(Request $request): AnonymousResourceCollection
     {
-        $query = Product::query();
-        if ($s = $request->input('s')) {
-            $query->whereRaw(sprintf("title LIKE '%%%s%%'", $s))
-                ->orWhereRaw(sprintf("description LIKE '%%%s%%'", $s));
-        }
+        return Cache::remember('products', 30, function () use ($request) {
+            sleep(5);
+            $query = Product::query();
+            if ($s = $request->input('s')) {
+                $query->whereRaw(sprintf("title LIKE '%%%s%%'", $s))
+                    ->orWhereRaw(sprintf("description LIKE '%%%s%%'", $s));
+            }
 
-        return ProductResource::collection($query->get());
+            return ProductResource::collection($query->get());
+        });
+
     }
 }
