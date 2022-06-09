a06297c2898c84e9e5545eb203b7f5cbe2f630c0
benjy8001 <benjy80@gmail.com>
2022-06-09 09:05:02 +0200

    React: Add react influencer

--- react-influencer/src/index.tsx
+++ react-influencer/src/index.tsx
@@ -3,23 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import axios from "axios";
-import configureStore from "./redux/configureStore";
-import {Provider} from "react-redux";
-
-axios.defaults.baseURL = 'https://backend.micro.test/api/admin/';
-axios.defaults.withCredentials = true;
-
-const store = configureStore();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      <Provider store={store}>
-          <App />
-      </Provider>
+    <App />
   </React.StrictMode>
 );
 
