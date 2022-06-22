4543da9efc84f6ca87ad692baf019dbaa4d86f6a
benjy8001 <benjy80@gmail.com>
2022-06-22 21:48:17 +0200

    Microservices: Influencer: Migrations

--- microservices/influencer/database/migrations/2022_05_13_201423_create_products_table.php
+++ microservices/influencer/database/migrations/2022_05_13_201423_create_products_table.php
@@ -13,13 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('products', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->primary();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('image');
+            $table->decimal('price');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
