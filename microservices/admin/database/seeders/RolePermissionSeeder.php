99a308bd85c919af62a11d80595bf41bf43586d4
benjy8001 <benjy80@gmail.com>
2022-06-27 15:18:17 +0200

    Microservices: Admin: Connect to Docker and add Seeders

---
+++ microservices/admin/database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Permission;
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $permissions = Permission::all();
+        $admin = Role::whereName('Admin')->first();
+        foreach ($permissions as $permission) {
+            DB::table(Role::ROLE_PERMISSION_TABLE_NAME)->insert([
+                'role_id' => $admin->id,
+                'permission_id' => $permission->id,
+            ]);
+        }
+
+        $editor = Role::whereName('Editor')->first();
+        foreach ($permissions as $permission) {
+            if (!in_array($permission->name, ['edit_roles'])) {
+                DB::table(Role::ROLE_PERMISSION_TABLE_NAME)->insert([
+                    'role_id' => $editor->id,
+                    'permission_id' => $permission->id,
+                ]);
+            }
+        }
+
+        $viewer = Role::whereName('Viewer')->first();
+        $viewerRoles = [
+            'view_users',
+            'view_roles',
+            'view_products',
+            'view_orders',
+        ];
+        foreach ($permissions as $permission) {
+            if (in_array($permission->name, $viewerRoles)) {
+                DB::table(Role::ROLE_PERMISSION_TABLE_NAME)->insert([
+                    'role_id' => $viewer->id,
+                    'permission_id' => $permission->id,
+                ]);
+            }
+        }
+    }
+}
---
+++ microservices/admin/database/seeders/microservices_user_roles.sql
@@ -0,0 +1,1 @@
+INSERT INTO admin.user_roles (id, user_id, role_id) VALUES (1, 1, 1);
